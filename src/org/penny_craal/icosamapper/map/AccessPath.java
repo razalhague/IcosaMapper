442770d268bcb2788494ad71ef4cdd77199f6d41
Ville Jokela <ville.jokela@penny-craal.org>
2013-04-17 16:07:40 +0300

    AccessPath now retains the full original path, and displays it in toString

--- src/org/penny_craal/icosamapper/map/AccessPath.java
+++ src/org/penny_craal/icosamapper/map/AccessPath.java
@@ -7,30 +7,44 @@ import java.util.Arrays;
  * @author Ville Jokela
  */
 public class AccessPath {
-    private byte[] path;
+    private byte [] path;
+    private int index;
     
     public AccessPath(byte[] path) {
         this.path = path;
+        index = 0;
+    }
+    
+    private AccessPath(byte[] path, int index) {
+        this.path = path;
+        this.index = index;
     }
     
     public byte head() {
-        return path[0];
+        return path[index];
     }
     
     public int length() {
-        return path.length;
+        return path.length - index;
     }
     
+    /**
+     * 
+     * @return an AccessPath with the first element of the path removed. 
+     */
     public AccessPath rest() {
-        return new AccessPath(Arrays.copyOfRange(path, 1, path.length));
+        return new AccessPath(path, index + 1);
     }
     
+    @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder("{ ");
         
-        sb.append("{ ");
-        for (byte b: path) {
-            sb.append(b);
+        for (int i = 0; i < path.length; i++) {
+            if (i == index) {
+                sb.append("CUR: ");
+            }
+            sb.append(path[i]);
             sb.append(", ");
         }
         sb.delete(sb.length() - 2, sb.length());
