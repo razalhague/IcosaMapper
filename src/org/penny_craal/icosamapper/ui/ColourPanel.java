54779b9e0255ea32137260733823cd58b3e755b6
Zamess <james.pearce@hotmail.com>
2013-04-22 17:09:37 +0300

    ColourPicker & ColouPanel

---
+++ src/org/penny_craal/icosamapper/ui/ColourPanel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.penny_craal.icosamapper.ui;
+
+import javax.swing.Icon;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+
+/**
+ *
+ * @author Ville Jokela & James Pearce
+ */
+public class ColourPanel extends AbstractColorChooserPanel {
+
+    public ColourPanel() {
+        
+    }
+    @Override
+    public void updateChooser() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void buildChooser() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getDisplayName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Icon getSmallDisplayIcon() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Icon getLargeDisplayIcon() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
--- src/org/penny_craal/icosamapper/ui/ColourPicker.java
+++ src/org/penny_craal/icosamapper/ui/ColourPicker.java
@@ -1,15 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.penny_craal.icosamapper.ui;
 
 import javax.swing.JColorChooser;
 
 /**
  *
- * @author Ville Jokela
+ * @author Ville Jokela & James Pearce
  */
 public class ColourPicker extends JColorChooser {
     
+    public ColourPicker() {
+        
+    }
 }
