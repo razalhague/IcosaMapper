0c0819c46873b2ffcd3a2cd8067e63bf6ef50ae7
Ville Jokela <ville.jokela@penny-craal.org>
2016-01-20 11:55:10 +0200

    PathTest now uses testNG like the other tests

--- src/test/java/org/penny_craal/icosamapper/map/PathTest.java
+++ src/test/java/org/penny_craal/icosamapper/map/PathTest.java
@@ -1,32 +1,33 @@
 package org.penny_craal.icosamapper.map;
 
-import junit.framework.TestCase;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 /**
  *
  * @author Ville Jokela
  */
-public class PathTest extends TestCase {
-    public PathTest(String testName) {
-        super(testName);
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+public class PathTest {
+    private byte[] path;
+
+    @BeforeMethod
+    public void setUp() throws InvalidPathException {
+        path = new byte[] {1,2,3,4,5};
     }
 
+    @AfterMethod
+    public void tearDown() {
+        path = null;
+    }
     /**
      * Test of first method, of class Path.
      */
+    @Test
     public void testFirst() {
         System.out.println("first");
-        byte[] path = {1,2,3,4,5};
         Path instance = new Path(path);
         for (int i = 0; i < path.length; i++, instance = instance.rest()) {
             assertEquals(path[i], instance.first());
@@ -36,9 +37,9 @@ public class PathTest extends TestCase {
     /**
      * Test of length method, of class Path.
      */
+    @Test
     public void testLength() {
         System.out.println("length");
-        byte[] path = {1,2,3,4,5};
         Path instance = new Path(path);
         for (int i = 0; i < path.length; i++, instance = instance.rest()) {
             assertEquals(path.length - i, instance.length());
@@ -48,6 +49,7 @@ public class PathTest extends TestCase {
     /**
      * Test of rest method, of class Path.
      */
+    @Test
     public void testRest() {
         System.out.println("rest");
         // the test for first() tests this method as well as it can be tested
@@ -57,9 +59,9 @@ public class PathTest extends TestCase {
     /**
      * Test of toString method, of class Path.
      */
+    @Test
     public void testToString() {
         System.out.println("toString");
-        byte[] path = {1,2,3,4,5};
         Path instance = new Path(path);
         assertEquals("{ CUR: 1, 2, 3, 4, 5 }", instance.toString());
     }
